6b495e730cd5c8e259a464d301c931f8641cccc5
yeray-yas <yerayarucas@gmail.com>
2024-01-11 23:11:43 +0000

    implemented a drawer layout side menu

--- app/src/main/java/com/yeray_yas/marvelsuperheroes/presentation/ui/jetpack_navigation/NavGraphActivity.kt
+++ app/src/main/java/com/yeray_yas/marvelsuperheroes/presentation/ui/jetpack_navigation/NavGraphActivity.kt
@@ -2,12 +2,15 @@ package com.yeray_yas.marvelsuperheroes.presentation.ui.jetpack_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.yeray_yas.marvelsuperheroes.R
 import androidx.navigation.findNavController
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NavGraphActivity : AppCompatActivity() {
 
@@ -20,14 +23,22 @@ class NavGraphActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.characterListFragment),
+            drawerLayout = drawerLayout
+        )
         setupActionBarWithNavController(
             navController = navController,
             configuration = appBarConfiguration
         )
 
+        navView.setupWithNavController(navController)
+
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration)
