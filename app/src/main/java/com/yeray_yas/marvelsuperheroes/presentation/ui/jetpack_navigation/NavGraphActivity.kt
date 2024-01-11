d36e1dd3faeca0ea0e7bb6efcedf127a1ea03a0b
yeray-yas <yerayarucas@gmail.com>
2024-01-11 19:47:47 +0000

    added back button to detail fragment

--- app/src/main/java/com/yeray_yas/marvelsuperheroes/presentation/ui/jetpack_navigation/NavGraphActivity.kt
+++ app/src/main/java/com/yeray_yas/marvelsuperheroes/presentation/ui/jetpack_navigation/NavGraphActivity.kt
@@ -3,9 +3,15 @@ package com.yeray_yas.marvelsuperheroes.presentation.ui.jetpack_navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.yeray_yas.marvelsuperheroes.R
+import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
 
 class NavGraphActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,17 @@ class NavGraphActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = appBarConfiguration
+        )
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
